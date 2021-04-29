777318936712f56585269f52a0315c907f969e43
AtmaWPN <63207425+AtmaWPN@users.noreply.github.com>
2021-04-29 13:04:56 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// Create the canvas
+var canvas = document.createElement("canvas");
+var ctx = canvas.getContext("2d");
+canvas.width = 912;
+canvas.height = 676;
+document.body.appendChild(canvas);
+var BLACK = "rgb(0, 0, 0)";
+canvas.fill(BLACK);
