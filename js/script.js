b83831824840ab70d816317d58e94cae0cb95642
AtmaWPN <63207425+AtmaWPN@users.noreply.github.com>
2021-04-29 13:54:40 -0500

    Set up the basic canvas and main loop

---
+++ js/script.js
@@ -0,0 +1,24 @@
+//set up the canvas
+var stage = document.getElementById("gameCanvas");
+stage.width = 900;
+stage.height = 600;
+var ctx = stage.getContext("2d");
+
+var main = function () {
+	//test code
+	ctx.fillStyle = "black";
+	ctx.fillRect(10, 10, 10, 10);
+	//"state machine"
+	switch(state) {
+		case "game":
+		break;
+		case "menu":
+		break;
+		default:
+		alert("state not found");
+	}
+	//important
+	requestAnimationFrame(main);
+}
+
+main();
--- script.js
+++
@@ -1,8 +0,0 @@
-// Create the canvas
-var canvas = document.createElement("canvas");
-var ctx = canvas.getContext("2d");
-canvas.width = 912;
-canvas.height = 676;
-document.body.appendChild(canvas);
-var BLACK = "rgb(0, 0, 0)";
-canvas.fill(BLACK);
